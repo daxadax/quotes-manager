edab98effb8ed58a2921c6743daa2c82949a6848
Dax <d.dax@email.com>
2014-12-02 21:32:47 +0100

    Add interface

--- lib/manager.rb
+++ lib/manager.rb
@@ -1,51 +1,22 @@
-require "manager/version"
+require 'manager/version'
+require 'manager/interface'
 require 'users'
 require 'quotes'
 
 module Manager
+  include Interface
 
   DOMAINS = [
     Users,
     Quotes
   ]
 
-  def self.create_quote(args)
-    quote = call_use_case(Quotes, :CreateQuote, args)
-
-    publish_quote_for_user(args[:user_uid], quote.uid) unless quote.error
-    quote
+  Quotes::ServiceFactory.register :quotes_backend do
+    Persistence::Gateways::QuotesGatewayBackend.new
   end
 
-  def self.method_missing(method, args = nil)
-    use_case = method.to_s.split('_').each(&:capitalize!).join.to_sym
-    domain = domain_for use_case
-
-    return call_use_case(domain, use_case, args) if domain
-    super
-  end
-
-  private
-
-  def self.call_use_case(domain, use_case, args)
-    use_case = eval("#{domain}::UseCases::#{use_case}.new(#{args})")
-    use_case.call
-  end
-
-  def self.publish_quote_for_user(user_uid, quote_uid)
-    input = {
-      :uid => user_uid,
-      :quote_uid => quote_uid
-    }
-
-    call_use_case(Users, :PublishQuote, input)
-  end
-
-  def self.domain_for(use_case)
-    DOMAINS.each do |domain|
-      use_cases = eval "#{domain}::UseCases.constants"
-      return domain if use_cases.include?(use_case)
-    end
-    raise ArgumentError, "You tried to call '#{use_case}', but it doesn't exist\n"
+  Quotes::ServiceFactory.register :publications_backend do
+    Persistence::Gateways::PublicationsGatewayBackend.new
   end
 
 end
---
+++ lib/manager/interface.rb
@@ -0,0 +1,80 @@
+module Manager
+  module Interface
+
+    def self.create_quote(args)
+      result = call_use_case Quotes, :CreateQuote, args
+
+      update_added(args[:user_uid], :quote, result.uid) unless result.error
+      result
+    end
+
+    def self.delete_quote(args)
+      result = call_use_case Quotes, :DeleteQuote, args
+
+      update_added(args[:user_uid], :quote, args[:uid]) unless result.error
+      result
+    end
+
+    def self.create_publication(args)
+      result = call_use_case Quotes, :CreatePublication, args
+
+      update_added(args[:user_uid], :publication, result.uid) unless result.error
+      result
+    end
+
+    def self.delete_publication(args)
+      result = call_use_case Quotes, :DeletePublication, args
+
+      update_added(args[:user_uid], :publication, args[:uid]) unless result.error
+      result
+    end
+
+    def self.authenticate_user(args)
+      result = call_use_case Users, :AuthenticateUser, args
+
+      unless result.error
+        call_use_case Users, :UpdateUser,
+          :uid => result.uid,
+          :auth_key => args[:auth_key],
+          :updates => {
+            :last_login_address => args[:login_data][:ip_address],
+            :last_login_time => Time.now.utc.to_i,
+            :update_login_count => true
+          }
+      end
+      result
+    end
+
+    def self.method_missing(method, args = nil)
+      use_case = method.to_s.split('_').each(&:capitalize!).join.to_sym
+      domain = domain_for use_case
+
+      return call_use_case(domain, use_case, args) if domain
+      raise ArgumentError "You tried to call #{method}, but it doesn't exist"
+    end
+
+    private
+
+    def self.call_use_case(domain, use_case, args)
+      use_case = eval("#{domain}::UseCases::#{use_case}.new(#{args})")
+      use_case.call
+    end
+
+    def self.update_added(user_uid, type, object_uid)
+      args = { :uid => user_uid }
+      args[:quote_uid] = object_uid if type == :quote
+      args[:publication_uid] = object_uid if type == :publication
+
+      call_use_case Users, :UpdateAdded, args
+    end
+
+    def self.domain_for(use_case)
+      DOMAINS.each do |domain|
+        use_cases = eval "#{domain}::UseCases.constants"
+        return domain if use_cases.include?(use_case)
+      end
+      raise ArgumentError, "You tried to call '#{use_case}', but it doesn't exist\n"
+    end
+
+  end
+end
